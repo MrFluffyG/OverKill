0666438d9537d8ef06040e1dca708853630384e7
MrFluffyG <36365625+MrFluffyG@users.noreply.github.com>
2018-03-12 18:50:52 +0200

    Izveidots maze un after game karte
    mazi ir izmantots hunt and kill algoritms kas palaišanas laikā izveido laberintu  izmantojot key values.

---
+++ OverKill/Assets/ProceduralNumberGenerator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProceduralNumberGenerator {
+	public static int currentPosition = 0;
+    //maze seeed
+	public const string key = "44411411232434132323444414111344422144313442312423242341212333321";
+
+	public static int GetNextNumber() {
+		string currentNum = key.Substring(currentPosition++ % key.Length, 1);
+		return int.Parse (currentNum);
+	}
+}
